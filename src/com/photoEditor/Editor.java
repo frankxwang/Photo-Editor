b53606f74fa3d8592545284ccf1fe119dcf53354
thegithubgeek <wfrankw9@gmail.com>
2016-10-08 15:07:38 -0700

    :)

--- src/com/photoEditor/Editor.java
+++ src/com/photoEditor/Editor.java
@@ -1,17 +1,22 @@
 package com.photoEditor;
 
 import javax.imageio.ImageIO;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Editor {
 	JFrame frame;
 	JFileChooser fc;
 	JButton open;
 	JButton save;
+	Panel panel;
+	boolean[] mouse = new boolean[MouseEvent.BUTTON3];
 	
 	public Editor() {
 		// TODO Auto-generated method stub
@@ -19,13 +24,15 @@ public class Editor {
 		
 		fc = new JFileChooser();
 		
-		JPanel panel = new Panel();
+		panel = new Panel();
+		panel.setSize(new Dimension(400, 400));
+		panel.setBackground(Color.green);
 		
 		open = new JButton("Open a File...");
-		open.addActionListener((Panel)panel);
+		open.addActionListener(panel);
 		
 		save = new JButton("Save a File...");
-		save.addActionListener((Panel)panel);
+		save.addActionListener(panel);
 		
 		frame.setPreferredSize(new Dimension(500, 500));
 		frame.setVisible(true);
@@ -35,30 +42,73 @@ public class Editor {
 		frame.add(save, BorderLayout.NORTH);
 		frame.add(open, BorderLayout.PAGE_END);
 		frame.pack();
+		Timer t = new Timer();
+		t.schedule(new Update(), 0, 10);
 	}
-	class Panel extends JPanel implements ActionListener{
+	class Update extends TimerTask{
+		@Override
+		public void run() {
+			(panel).repaint();
+		}
+	}
+	class Panel extends JPanel implements ActionListener, MouseListener{
+		Panel(){
+			addMouseListener(this);
+		}
 		Graphics graphics = null;
+		BufferedImage img;
 		public void actionPerformed(ActionEvent e) {
 			if(e.getSource() == open){
 				int returnVal = fc.showOpenDialog(frame);
 				if (returnVal == JFileChooser.APPROVE_OPTION) {
 		            File file = fc.getSelectedFile();
-		            BufferedImage img;
 		            try{
 		            	img = ImageIO.read(file);
-		            	graphics = img.getGraphics();
+		            	frame.setSize(new Dimension(img.getWidth(), img.getHeight()));
+		            	graphics = img.createGraphics();
 		            }catch(Exception er){
 		            	er.printStackTrace();
 		            }
 		        }
+			}else if(e.getSource() == save){
+				int returnVal = fc.showOpenDialog(frame);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					try {
+						ImageIO.write(img, "png", new File(fc.getCurrentDirectory().toString() 
+								+ "/savedimg.png"));
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
 			}
 		}
 		protected void paintComponent(Graphics g){
-			if(graphics != null){
-				g = graphics;
-				graphics = null;
-				System.out.println("ye");
+			if(mouse[MouseEvent.BUTTON1]){
+				Point p = MouseInfo.getPointerInfo().getLocation();
+				graphics.setColor(Color.black);
+				int width=50;
+				int height=50;
+				graphics.fillOval(p.x-width/2, p.y-height/2, width, height);
 			}
+			g.drawImage(img, 0, 0, null);
+		}
+		@Override
+		public void mouseClicked(MouseEvent e) {
+		}
+		@Override
+		public void mousePressed(MouseEvent e){
+			mouse[e.getButton()] = true;
+		}
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			mouse[e.getButton()] = false;
+		}
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
 		}
 	}
 }
